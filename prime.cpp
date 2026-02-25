5a2b412a8dd36936968cb39e0d2ceeacd8e53b48
Bhavika Khabya <bhavikakhabya@gmail.com>
2026-02-25 10:25:25 +0530

    Added prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+
+    cout << "Prime numbers from 1 to 100 are:" << endl;
+
+    for (n = 2; n <= 100; n++) {
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                break;
+            }
+        }
+        if (i == n) {
+            cout << n << endl;
+        }
+    }
+
+    return 0;
+}
